4da67a3da20dfbb9665e814ae1aef0416419a0e7
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-01-25 23:20:22 -0300

    projeto pizzaria
    coloquei comentários no código.

--- projeto-python-mysql/projetoPizza/main.py
+++ projeto-python-mysql/projetoPizza/main.py
@@ -1,12 +1,16 @@
-import mysql.connector
+import mysql.connector           # importando o banco de dados
 from usuario import Usuario
-from pizza import Pizza
+from pizza import Pizza          # importando class da linha 2 até 5
 from bebidas import Bebidas
-from datetime import date
-import os
+from vendas import Vendas
+from datetime import date        # importando data
+import getpass                   # gestpass para esconder senha
+import os                        # para limpar tela
 
 today = date.today()
-print("Today's date:", today)
+print("Today's date:", today)          # estou chamando é mostrando a data
+
+# função pizzaria para guarda a marca do cliente
 
 
 def pizzaria():
@@ -14,35 +18,40 @@ def pizzaria():
     print('             ##     PIZZARIA GO-IT    ##')
     print('================================================================')
 
+# função menu para guarda o menu do código onde esse menu tem 4 opção
+
 
 def menu():
     print('''
                MENU:
 
                [1] - Iniciar Vendas
-               [2] - Cadastra Usuario
+               [2] - Cadastrar Usuario
                [3] - Total de Vendas
                [4] - Desligar
 
              ===================================''')
 
 
+# chamando função pizzaria e menu
 pizzaria()
 menu()
-n1 = input('Escolha uma opção: ')
-os.system('cls')
-cont01 = 0
+escolhaMenu = input('Escolha uma opção: ')  # escolher uma opção do menur
+os.system('cls')               # chamando limpa tela
+cont01 = 0   # variavel para usar como contador
 cont02 = 0
-if (n1 == '1'):
+if (escolhaMenu == '1'):
 
-    while True:
+    while True:  # loop de repetição, enquanto for verdadeira vai rodar
         os.system('cls')
         pizzaria()
 
-        inputNomeUsuario = input('QUAL E SEU LOGIN--> ')
+        inputNomeUsuario = input('Seu login--> ')
 
-        inputSenha = input('QUAL É SUA SENHA --> ')
+        # chamando o getpass, senha escondida
+        inputSenha = getpass.getpass('Sua senha --> ')
 
+        # linha 55 áte 64 conexão com o banco pizzariagoit1 na tabela tb_usuarios
         cnx = mysql.connector.connect(
             host='localhost', database='pizzariagoit1', user='root', password='')
         cursor = cnx.cursor()
@@ -53,24 +62,24 @@ if (n1 == '1'):
 
         cursor.close()
         cnx.close()
-
+        # chamando a class Usuario, modificando para objeto
         usuario = Usuario(0, "", "", "")
 
-        if queryResult == None:
+        if queryResult == None:     # usuaria sendo incorreto cai nesse if
             erroUsuario = input("** Usuario incorreto **  [enter]")
-            cont01 = cont01 + 1
+            cont01 = cont01 + 1     # somando o contador chegar a 3 sai do programa
             if cont01 == 3:
                 break
 
-            # repetir = input('você quer tentar de novo (s)sim ou (n)não ')
-
-        else:
+        else:  # usuario correto cai nesse else
             usuario = Usuario(
                 queryResult[0], queryResult[2], queryResult[3], queryResult[1])
 
-        if inputSenha == usuario.senha:
+        if inputSenha == usuario.senha:  # senha correta cai nesse if
+            # usuario e senha correta roda o programa
             print("logado com sucesso !")
 
+            # linha 82 áte 93 conexão com o banco pizzariagoit1 na tabela tb_pizza
             cnx = mysql.connector.connect(
                 host='localhost', database='pizzariagoit1', user='root', password='')
             cursor = cnx.cursor()
@@ -82,24 +91,25 @@ if (n1 == '1'):
             cnx.close()
 
             print(queryResult)
-
+            # uma lista
             pizzas = []
-
+            # para pegar todo o conteúdo
             for i in queryResult:
-
+                # coloca conteúdo na lista
                 pizzas.append(Pizza(i[0], i[1], i[2]))
                 os.system('cls')
                 pizzaria()
-
+             # printa conteúdo
             for pizza in pizzas:
                 print(f'{pizza.id}        {pizza.nome}                {pizza.preco}')
                 print('------------------------------------------------------')
 
+             # linha 108 áte 121 conexão com o banco pizzariagoit1 na tabela tb_bebidas
             cnx = mysql.connector.connect(
                 host='localhost', database='pizzariagoit1', user='root', password='')
             cursor = cnx.cursor()
 
-            idPizzaSelecionada = input("Digite o numero pizza: ")
+            idPizzaSelecionada = input(" Id da pizza --> ")
 
             cursor.execute("SELECT * FROM tb_bebidas")
 
@@ -111,7 +121,7 @@ if (n1 == '1'):
             bebidas = []
 
             for i in queryResult:
-                bebidas.append(Bebidas(i[0], i[1], i[3]))
+                bebidas.append(Bebidas(i[0], i[1], i[2]))
 
             os.system('cls')
             pizzaria()
@@ -121,14 +131,20 @@ if (n1 == '1'):
                     f'{bebida.id}        {bebida.nome}                {bebida.preco}')
                 print('------------------------------------------------------')
 
-            idBebidaSelecionada = input("Digite o numero da bebida: ")
+            idBebidaSelecionada = input("Id bebida --> ")
+            # linha 137 áte 153 conexão com o banco pizzariagoit1 na tabela tb_vendas
 
             cnx = mysql.connector.connect(
                 host='localhost', database='pizzariagoit1', user='root', password='')
             cursor = cnx.cursor()
 
+            nomeCliente = input('Nome do cliente --> ')
+            dataPedido = date.today()
+            # colocando tudo que foi vendido no banco de dados tabala tb_venda
             add_venda = (
-                f'INSERT INTO tb_vendas(id_pizza, totalpagar, nomecliente, id_bebidas, dataCompra ) VALUES("{pizzas[int(idPizzaSelecionada) - 1].id}", {pizzas[int(idPizzaSelecionada) - 1].preco + bebidas[int(idBebidaSelecionada)-1].preco}, "beatriz",{bebidas[int(idBebidaSelecionada)-1].id}, {date.today()})')
+                f'INSERT INTO tb_vendas(id_pizza, totalpagar, nomecliente, id_bebidas, datavenda ) VALUES("{pizzas[int(idPizzaSelecionada) - 1].id}", {pizzas[int(idPizzaSelecionada) - 1].preco + bebidas[int(idBebidaSelecionada)-1].preco}, "{nomeCliente}",{bebidas[int(idBebidaSelecionada)-1].id}, now())')
+
+            vendaOk = input('venda ok.. [enter]')
 
             cursor.execute(add_venda)
             cnx.commit()
@@ -136,7 +152,13 @@ if (n1 == '1'):
             cursor.close()
             cnx.close()
 
-        else:
+            # esse if e para continuar vendedo ou termina o programa
+            continuarVenda = input(
+                ' Continuar vendendo [s]sim ou [n]não ? --> ')
+            if continuarVenda == 'n':
+                break
+
+        else:   # cai nesse else senha incorretas
             erroSenha = input("** Senha incorreta **  [enter]")
             print('--------------------------------------------')
             print('')
@@ -146,22 +168,23 @@ if (n1 == '1'):
             if cont02 == 3:
                 break
 
-
-if (n1 == '2'):
+# menu para cadastrar novos usuarios
+if (escolhaMenu == '2'):
     os.system('cls')
     pizzaria()
 
     conexao = mysql.connector.connect(
-        host="localhost",
+        host="localhost",          
         user="root",
         password="",
         database="pizzariagoit1"
     )
     cursor = conexao.cursor()
 
-    funcao = input('Qual é a função: ')
-    login_usuario = input('Qual é o nome do usuario: ')
-    senha_usuario = input('Qual é a senha: ')
+    # passar informação do novo usuario
+    funcao = input('função --> ')
+    login_usuario = input('Login do usuario --> ')
+    senha_usuario = input('Senha nova --> ')
 
     comando = f'INSERT INTO tb_usuarios (funcao, login_usuario, senha_usuario) VALUES ("{funcao}", "{login_usuario}", "{senha_usuario}")'
 
@@ -171,8 +194,8 @@ if (n1 == '2'):
     cursor.close()
     conexao.close()
 
-
-elif (n1 == '3'):
+# mostra o total de vendas
+elif (escolhaMenu == '3'):
     os.system('cls')
     pizzaria()
     conexao = mysql.connector.connect(
@@ -186,11 +209,23 @@ elif (n1 == '3'):
     comando = f'SELECT * FROM tb_vendas'
     cursor.execute(comando)
     resultado = cursor.fetchall()
-    print(resultado)
+
+    vendas = []
+
+    for i in resultado:
+        vendas.append(Vendas(i[0], i[1], i[2], i[3], i[4], i[5]))
 
     cursor.close()
     conexao.close()
 
+    os.system('cls')
+
+    print('id venda      | id bebida     | id pizza     | valor     | clinte      | data     ')
+    for venda in vendas:
+        print(f'{venda.id}             | {venda.idBebida}             | {venda.idPizza}            | {venda.valor}      | {venda.cliente}       | {venda.data}')
+    print('------------------------------------------------------')
+
+# quando cai nesse else é o fim do programa
 else:
     os.system('cls')
     pizzaria()
--- projeto-python-mysql/projetoPizza/vendas.py
+++ projeto-python-mysql/projetoPizza/vendas.py
@@ -1,112 +1,63 @@
 class Vendas:
-    def __init__(self, id_usuarios, id_vendas,  id_bebidas, id_pizza,  id_entrega, nomeCliente, totalPagar, qtdeTotalPizzas, qtdeTotalBebidas, dataCompra):
-        self.__id_usuarios: id_usuarios
-        self.__id_vendas: id_vendas
-        self.__id_bebidas: id_bebidas
-        self.__id_pizza: id_pizza
-        self.__id_entrega: id_entrega
-        self.__nomeCliente: nomeCliente
-        self.__totalPagar: totalPagar
-        self.__qtdeTotalPizzas: qtdeTotalPizzas
-        self.__qtdeTotalBebidas: qtdeTotalBebidas
-        self.__dataCompra: dataCompra
+    __id: int
+    __idBebida: int
+    __idPizza: int
+    __cliente: str
+    __valor: float
+    __data: str
 
+    def __init__(self, id, idBebida, idPizza, cliente, valor, data):
+        self.__cliente = cliente
+        self.__valor = valor
+        self.__idBebida = idBebida
+        self.__idPizza = idPizza
+        self.__id = id
+        self.__data = data
 
-@property
-def id_usuarios(self):
-    return self.__id_usuarios
+    @property
+    def data(self):
+        return self.__data
 
+    @data.setter
+    def data(self, data):
+        self.__data = data
 
-@id_usuarios.setter
-def id_usuarios(self, id_usuarios):
-    self.__id_usuarios = id_usuarios
+    @property
+    def cliente(self):
+        return self.__cliente
 
+    @cliente.setter
+    def cliente(self, cliente):
+        self.__cliente = cliente
 
-@property
-def id_vendas(self):
-    return self.__id_vendas
+    @property
+    def valor(self):
+        return self.__valor
 
+    @valor.setter
+    def valor(self, valor):
+        self.__valor = valor
 
-@id_vendas.setter
-def id_vendas(self, id_vendas):
-    self.__id_vendas = id_vendas
+    @property
+    def id(self):
+        return self.__id
 
+    @id.setter
+    def id(self, id):
+        self.__id = id
 
-@property
-def id_bebidas(self):
-    return self.__id_bebidas
+    @property
+    def idPizza(self):
+        return self.__idPizza
 
+    @idPizza.setter
+    def idPizza(self, idPizza):
+        self.__idPizza = idPizza
 
-@id_bebidas.setter
-def id_bebidas(self, id_bebidas):
-    self.__id_bebidas = id_bebidas
+    @property
+    def idBebida(self):
+        return self.__idBebida
 
-
-@property
-def id_pizza(self):
-    return self.__id_pizza
-
-
-@id_pizza.setter
-def id_pizza(self, id_pizza):
-    self.__id_pizza = id_pizza
-
-
-@property
-def id_entrega(self):
-    return self.__id_entrega
-
-
-@id_entrega.setter
-def id_entrega(self, id_entrega):
-    self.__id_entrega = id_entrega
-
-
-@property
-def nomeCliente(self):
-    return self.__nomeCliente
-
-
-@nomeCliente.setter
-def nomeCliente(self, nomeCliente):
-    self.__nomeCliente = nomeCliente
-
-
-@property
-def totalPagar(self):
-    return self.__totalPagar
-
-
-@totalPagar.setter
-def totalPagar(self, totalPagar):
-    self.__totalPagar = totalPagar
-
-
-@property
-def qtdeTotalPizzas(self):
-    return self.__qtdeTotalPizzas
-
-
-@qtdeTotalPizzas.setter
-def qtdeTotalPizzas(self, qtdeTotalPizzas):
-    self.__qtdeTotalPizzas = qtdeTotalPizzas
-
-
-@property
-def qtdeTotalBebidas(self):
-    return self.__qtdeTotalBebidas
-
-
-@qtdeTotalBebidas.setter
-def nomecliente(self, qtdeTotalBebidas):
-    self.__qtdeTotalBebidas = qtdeTotalBebidas
-
-
-@property
-def dataCompra(self):
-    return self.__dataCompra
-
-
-@dataCompra.setter
-def dataCompra(self, dataCompra):
-    self.__dataCompra = dataCompra
+    @idBebida.setter
+    def idBebida(self, idBebida):
+        self.__idBebida = idBebida
